d5d8bd0418cc82cf7140e8571b36a7c7f4cd8057
zly123987 <zhanglvyesnest@gmail.com>
2021-03-26 22:37:18 +0800

    update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import csv
 import json
+import random
+
 from bs4 import BeautifulSoup as bs
 import pymongo
 import requests
 import time
 import download_html
-
+from selenium import webdriver
 MONGO_HOST = '119.8.190.75'
 MONGO_PORT = 8635
 MONGO_USER = 'rwuser'
@@ -22,31 +24,34 @@ def save_namelist():
     open('all_name_03_24.json', 'w').write(json.dumps(list(set(out))))
 
 def parse_html(html_doc):
+    # Put in the Pathline for the chromedriver
+    driver = webdriver.Chrome('/home/lyuye/workspace/maven_migration/chromedriver')
+    driver.implicitly_wait(30)
+    # inputting the address to extract info
 
-    download_html.download_html('vulLibName.csv')
-    # with open('migration_map.csv', 'a') as f:
-    #     writer = csv.writer(f)
-    #     names = json.load(open('all_name_03_24.json'))
-    #     print(len(names))
-    #     # names = ['org.jclouds.labs|google-compute']
-    #     for name in names[:100]:
-    #         time.sleep(1)
-    #         g, a = name. split('|')
-    #         rep = requests.get('https://mvnrepository.com/artifact/' + g + '/' + a)
-    #         if rep.status_code!=200:
-    #             continue
-    #         content = rep.text
-    #         with open(f'htmls/{g}|{a}.html', 'w') as wf:
-    #             wf.write(content)
-    #         soup = bs(content, 'html.parser')
-    #         for b in soup.find_all('b', text='Note'):
-    #             if b.get_text() == 'Note' and 'This artifact was moved to' in b.next_sibling:
-    #                 writer.writerow([name, '|'.join([e.get_text() for e in b.parent.find_all('a')])])
-    #                 print(g, a)
-    #                 break
-    #         print(names.index(name), '\r', end='', flush=True)
+    with open('migration_map.csv', 'a') as f:
+        writer = csv.writer(f)
+        names = list(csv.reader(open('vulLibName.csv')))
+        print(len(names))
+        # names = ['org.jclouds.labs|google-compute']
+        for name in names[200:231]:
+            g, a = name
+            driver.get('https://mvnrepository.com/artifact/'+g+'/'+a)
+            # input 5 second wait to load everything before capturing the current page
+            time.sleep(random.randrange(2, 5))
+            soup =bs(driver.page_source,'html.parser')
+            content = driver.page_source
+            with open(f'htmls/{g}|{a}.html', 'w') as wf:
+                wf.write(content)
+            for b in soup.find_all('b', text='Note'):
+                if b.get_text() == 'Note' and 'This artifact was moved to' in b.next_sibling:
+                    writer.writerow([name, '|'.join([e.get_text() for e in b.parent.find_all('a')])])
+                    print(g, a)
+                    break
+            print(names.index(name), '\r', end='', flush=True)
 
 
+    driver.close()
 
 
 if __name__=='__main__':
